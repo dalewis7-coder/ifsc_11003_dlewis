48bad2ce1271bc7103087e1f1f61cd07c384ec38
dalewis7-coder <dalewis7@ualr.edu>
2026-05-07 15:54:44 +0000

    Finshed up my revisions on final project

---
+++ slick-demo/assets/js/scripts.js
@@ -0,0 +1,11 @@
+$('.fade').slick({
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+  autoplaySpeed: 4000,
+  pauseOnHover: true,
+  pauseOnFocus: true,
+  infinite: true,
+  arrows: true,
+  dots: true
+});
